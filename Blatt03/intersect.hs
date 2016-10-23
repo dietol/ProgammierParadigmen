088b29b824abbf0465fd4edc5df42a3c1401a1ed
Oliver Dietz <oliver.dietz@student.kit.edu>
2016-10-23 19:49:56 -0400

    Finish Blatt03

--- Blatt03/intersect.hs
+++ Blatt03/intersect.hs
@@ -1,3 +1,18 @@
-intersect :: (Ord t) => [t] -> [t] -> [t]
-intersect xs ys = filter (`isInSec` ys) xs
+intersectU :: (Ord t) => [t] -> [t] -> [t]
+intersectU xs ys = filter (`isInSec` ys) xs
 		where isInSec x zs= x `elem` zs
+
+intersectAll :: (Ord t) => [[t]] -> [t]
+intersectAll (x:xs) = foldl (\acc y -> intersect acc y) x xs
+
+intersect :: (Ord t) => [t] -> [t] -> [t]
+intersect [] _ = []
+intersect _ [] = []
+intersect (x:xs) (y:ys)
+	| x<y = intersect xs (y:ys)
+	| x>y = intersect (x:xs) ys
+	| otherwise = [x] ++ (intersect xs ys) 
+
+commonMultiples :: Int -> Int -> Int -> [Int]
+commonMultiples a b c = intersectAll [[a,a*2..],[b,b*2..],[c,c*2..]]
+
